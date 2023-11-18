120035460fba8a84e91de532e740ce599f78a646
Julius van Voorden <juliusvanvoorden@outlook.com>
2023-11-19 00:51:29 +0100

    builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [wasm(), topLevelAwait()],
 });
